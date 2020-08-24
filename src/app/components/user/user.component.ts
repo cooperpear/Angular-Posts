f27a6a0fcdde13e2d13b2c7cdb8e0949b54c61ce
Cooper Pearson <mptprogesterone@gmail.com>
2020-08-24 13:03:34 -0500

    back up

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,23 @@
+//import modules
+import { Component } from '@angular/core';
+
+//Decorator
+@Component({
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css'],
+
+})
+
+//create class
+export class UserComponent {
+    //Properties
+    firstName = 'John';
+    lastName = 'Doe';
+    age = 30;
+
+    //Methods
+    constructor() {
+
+    }
+}
